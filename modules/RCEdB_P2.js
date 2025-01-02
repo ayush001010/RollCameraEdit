fab24b2919f88016dfc116ab7b7e5d37102bf4ca
Ayush Chaurasiya <23je0194@iitism.ac.in>
2025-01-02 21:14:19 +0530

    Update RCEdB_P2.js

--- modules/RCEdB_P2.js
+++ modules/RCEdB_P2.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
+const db = 'mongodb+srv://heyayush194:%40Yush869@cluster0.bmmlb.mongodb.net/RCEmediaDB?retryWrites=true&w=majority';
 
-mongoose.connect(`mongodb://localhost:27017/RCEmediaDB`);
+mongoose.set('debug', true);
+mongoose.connect(db)
+    .then(() => {
+        console.log('RCEmedia Rolling Database_AllProtfolio !!!');
+    })
+    .catch((err) => {
+        console.error('RCEmedia Cut !!!', err.message);
+    });
 
 const userSchema = mongoose.Schema({
     ProjectClient: String,
