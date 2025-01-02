410892b429e95998f83a2e60fb5929e1b124c029
Ayush Chaurasiya <23je0194@iitism.ac.in>
2025-01-02 21:13:50 +0530

    Update RCEdB_P.js

--- modules/RCEdB_P.js
+++ modules/RCEdB_P.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
+const db = 'mongodb+srv://heyayush194:%40Yush869@cluster0.bmmlb.mongodb.net/RCEmediaDB?retryWrites=true&w=majority';
 
-mongoose.connect(`mongodb://localhost:27017/RCEmediaDB`);
+mongoose.set('debug', true);
+mongoose.connect(db)
+    .then(() => {
+        console.log('RCEmedia Rolling Database_Protfolio !!!');
+    })
+    .catch((err) => {
+        console.error('RCEmedia Cut !!!', err.message);
+    });
 
 const userSchema = mongoose.Schema({
     ProjectTitle: String,
